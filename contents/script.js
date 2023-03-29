0320f8a707d23a85cdc9bfbd05aec24428ea5a45
Jainprashuk <90614495+Jainprashuk@users.noreply.github.com>
2023-03-30 02:35:21 +0530

    files uploaded

---
+++ contents/script.js
@@ -0,0 +1,220 @@
+function copyToClipboard() {
+    var myDivText = document.getElementById("myDiv").innerText;
+    navigator.clipboard.writeText(myDivText)
+      .then(() => {
+        console.log('Text copied to clipboard');
+        document.getElementById("copyBtn").innerText = "Copied"
+      })
+      .catch((error) => {
+        console.error('Could not copy text: ', error);
+      });
+  }
+
+  function copyToClipboard1() {
+    var myDivText = document.getElementById("myDiv1").innerText;
+    navigator.clipboard.writeText(myDivText)
+      .then(() => {
+        console.log('Text copied to clipboard');
+        document.getElementById("copyBtn1").innerText = "Copied"
+      })
+      .catch((error) => {
+        console.error('Could not copy text: ', error);
+      });
+  }
+  
+  function copyToClipboard2() {
+    var myDivText = document.getElementById("myDiv2").innerText;
+    navigator.clipboard.writeText(myDivText)
+      .then(() => {
+        console.log('Text copied to clipboard');
+        document.getElementById("copyBtn2").innerText = "Copied"
+      })
+      .catch((error) => {
+        console.error('Could not copy text: ', error);
+      });
+  }
+
+  function copyToClipboard3() {
+    var myDivText = document.getElementById("myDiv3").innerText;
+    navigator.clipboard.writeText(myDivText)
+      .then(() => {
+        console.log('Text copied to clipboard');
+        document.getElementById("copyBtn3").innerText = "Copied"
+      })
+      .catch((error) => {
+        console.error('Could not copy text: ', error);
+      });
+  }
+
+  function copyToClipboard4() {
+    var myDivText = document.getElementById("box1").innerText;
+    navigator.clipboard.writeText(myDivText)
+      .then(() => {
+        console.log('Text copied to clipboard');
+        document.getElementById("copyBtn4").innerText = "Copied"
+      })
+      .catch((error) => {
+        console.error('Could not copy text: ', error);
+      });
+  }
+
+  function copyToClipboard5() {
+    var myDivText = document.getElementById("box2").innerText;
+    navigator.clipboard.writeText(myDivText)
+      .then(() => {
+        console.log('Text copied to clipboard');
+        document.getElementById("copyBtn5").innerText = "Copied"
+      })
+      .catch((error) => {
+        console.error('Could not copy text: ', error);
+      });
+  }
+
+  function copyToClipboard6() {
+    var myDivText = document.getElementById("box3").innerText;
+    navigator.clipboard.writeText(myDivText)
+      .then(() => {
+        console.log('Text copied to clipboard');
+        document.getElementById("copyBtn6").innerText = "Copied"
+      })
+      .catch((error) => {
+        console.error('Could not copy text: ', error);
+      });
+  }
+
+
+  function copyToClipboard7() {
+    var myDivText = document.getElementById("loopb1").innerText;
+    navigator.clipboard.writeText(myDivText)
+      .then(() => {
+        console.log('Text copied to clipboard');
+        document.getElementById("copyBtn7").innerText = "Copied"
+      })
+      .catch((error) => {
+        console.error('Could not copy text: ', error);
+      });
+  }
+
+
+  function copyToClipboard8() {
+    var myDivText = document.getElementById("loopb2").innerText;
+    navigator.clipboard.writeText(myDivText)
+      .then(() => {
+        console.log('Text copied to clipboard');
+        document.getElementById("copyBtn8").innerText = "Copied"
+      })
+      .catch((error) => {
+        console.error('Could not copy text: ', error);
+      });
+  }
+
+  function copyToClipboard9() {
+    var myDivText = document.getElementById("loopb3").innerText;
+    navigator.clipboard.writeText(myDivText)
+      .then(() => {
+        console.log('Text copied to clipboard');
+        document.getElementById("copyBtn9").innerText = "Copied"
+      })
+      .catch((error) => {
+        console.error('Could not copy text: ', error);
+      });
+  }
+
+
+  function copyToClipboard10() {
+    var myDivText = document.getElementById("loopb4").innerText;
+    navigator.clipboard.writeText(myDivText)
+      .then(() => {
+        console.log('Text copied to clipboard');
+        document.getElementById("copyBtn10").innerText = "Copied"
+      })
+      .catch((error) => {
+        console.error('Could not copy text: ', error);
+      });
+  }
+
+  function copyToClipboard11() {
+    var myDivText = document.getElementById("loopb5").innerText;
+    navigator.clipboard.writeText(myDivText)
+      .then(() => {
+        console.log('Text copied to clipboard');
+        document.getElementById("copyBtn11").innerText = "Copied"
+      })
+      .catch((error) => {
+        console.error('Could not copy text: ', error);
+      });
+  }
+
+  function copyToClipboard12() {
+    var myDivText = document.getElementById("loopb6").innerText;
+    navigator.clipboard.writeText(myDivText)
+      .then(() => {
+        console.log('Text copied to clipboard');
+        document.getElementById("copyBtn12").innerText = "Copied"
+      })
+      .catch((error) => {
+        console.error('Could not copy text: ', error);
+      });
+  }
+
+  function copyToClipboard13() {
+    var myDivText = document.getElementById("loopb7").innerText;
+    navigator.clipboard.writeText(myDivText)
+      .then(() => {
+        console.log('Text copied to clipboard');
+        document.getElementById("copyBtn13").innerText = "Copied"
+      })
+      .catch((error) => {
+        console.error('Could not copy text: ', error);
+      });
+  }
+
+
+
+  function copyToClipboardclo() {
+    var myDivText = document.getElementById("myDivclo").innerText;
+    navigator.clipboard.writeText(myDivText)
+      .then(() => {
+        console.log('Text copied to clipboard');
+        document.getElementById("copyBtnclo").innerText = "Copied"
+      })
+      .catch((error) => {
+        console.error('Could not copy text: ', error);
+      });
+  }
+
+  function copyToClipboardclo1() {
+    var myDivText = document.getElementById("myDivclo1").innerText;
+    navigator.clipboard.writeText(myDivText)
+      .then(() => {
+        console.log('Text copied to clipboard');
+        document.getElementById("copyBtnclo1").innerText = "Copied"
+      })
+      .catch((error) => {
+        console.error('Could not copy text: ', error);
+      });
+  }
+
+  function copyToClipboardclo2() {
+    var myDivText = document.getElementById("myDivclo2").innerText;
+    navigator.clipboard.writeText(myDivText)
+      .then(() => {
+        console.log('Text copied to clipboard');
+        document.getElementById("copyBtnclo2").innerText = "Copied"
+      })
+      .catch((error) => {
+        console.error('Could not copy text: ', error);
+      });
+  }
+
+  function copyToClipboardclo3() {
+    var myDivText = document.getElementById("myDivclo3").innerText;
+    navigator.clipboard.writeText(myDivText)
+      .then(() => {
+        console.log('Text copied to clipboard');
+        document.getElementById("copyBtnclo3").innerText = "Copied"
+      })
+      .catch((error) => {
+        console.error('Could not copy text: ', error);
+      });
+  }
